3f0c6c2493f88eae90850469e1ab31161b656f6b
Carl Wicker <carlwicker@gmail.com>
2024-04-15 12:29:13 +0100

    testing resend

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { EmailTemplate } from "../../../components/EmailTemplate";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API);
 
 export async function POST() {
   try {
