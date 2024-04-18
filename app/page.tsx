e01c87fecd8007104c97eed68cfd6c26507ca74f
Carl Wicker <carlwicker@gmail.com>
2024-04-18 15:40:32 +0100

    home header hotfix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="py-32">
       <section className="flex w-full justify-center">
         <div className="xl:w-2/3 w-full align-middle flex flex-col self-center gap-8">
-          <h1 className="text-6xl font-bold uppercase">
+          <h1 className="lg:text-6xl text-4xl font-bold uppercase">
             Crafting Tomorrow&apos;s Anthems
           </h1>
           <p className="text-3xl">
